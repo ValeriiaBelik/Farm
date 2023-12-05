a451be9eaca5962f37d890de333d22bb3f416526
Valeriia Bielik <v.v.bielik@student.khai.edu>
2023-12-05 18:26:59 +0100

    init

---
+++ AnimalType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFarm
+{
+    public enum AnimalType
+    {
+        Chicken,
+        Cow,
+        Pig,
+        Sheep
+    }
+}
---
+++ PlantType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFarm
+{
+    public enum PlantType
+    {
+        Wheat,
+        Corn,
+        Carrot,
+        Tomato,
+        Pumpkin
+    }
+}
